77b5c77d8fd1f971391c7c4fcf4634137eb8f7ce
MystiaFin <cytrsx01@gmail.com>
2024-11-11 16:21:15 +0700

    fixed icon for forecast

--- src/controller/HomeController.js
+++ src/controller/HomeController.js
@@ -1,3 +1,16 @@
+const weatherIconMap = {
+  Clear: "src/assets/w-clear.svg",
+  Rain: "src/assets/w-drizzle-1.svg",
+  Clouds: "src/assets/w-cloud-1.svg",
+  Snow: "src/assets/w-snow.svg",
+  Thunderstorm: "src/assets/w-thunderstorm-1.svg",
+  Drizzle: "src/assets/w-drizzle-2.svg",
+  Mist: "src/assets/w-cloud-2.svg",
+  Smoke: "src/assets/w-cloud-3.svg",
+  Dust: "src/assets/w-cloud-3.svg",
+  Fog: "src/assets/w-cloud-3.svg",
+};
+
 app.controller("HomeController", function ($scope) {
   $scope.selectedCity = "Jakarta";
   $scope.cities = ["Jakarta", "New York"];
@@ -15,11 +28,17 @@ app.controller("HomeForecast", function ($scope, WeatherService) {
     $scope.loading = true;
     WeatherService.getForecastByCity($scope.$parent.selectedCity)
       .then(function (response) {
-        $scope.forecast = response.data.list.map((item) => ({
-          weather: item.weather[0].main,
-          icon: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`,
-          date: new Date(item.dt * 1000),
-        }));
+        $scope.forecast = response.data.list.map((item) => {
+          const weatherMain = item.weather[0].main;
+          const localIcon = weatherIconMap[weatherMain];
+          const defaultIcon = `http://openweathermap.org/img/w/${item.weather[0].icon}.png`;
+
+          return {
+            weather: weatherMain,
+            icon: localIcon ? localIcon : defaultIcon,
+            date: new Date(item.dt * 1000),
+          };
+        });
         $scope.loading = false;
       })
       .catch(function (error) {
