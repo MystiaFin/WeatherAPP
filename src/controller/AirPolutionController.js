1c9f64951f94573083723e794d4933498b10d0ce
yusril <yusril.535239202@stu.untar.ac.id>
2024-11-30 17:05:59 +0700

    Upgrade UI and Date

--- src/controller/AirPolutionController.js
+++ src/controller/AirPolutionController.js
@@ -1,26 +1,32 @@
-app.controller("AirPolutionController", function ($scope, AirPolutionService) {
+app.controller("AirPolutionController", function ($scope, AirPolutionService, $interval) {
   $scope.selectedCity = "Jakarta";
   $scope.cities = ["Jakarta", "New York"];
   
+
+  $interval(function () {
+    $scope.currentDate = new Date();
+  }, 1000);
+
   $scope.changeCity = function (city) {
-  $scope.selectedCity = city;
-  loadAirPollution();
+    $scope.selectedCity = city;
+    loadAirPollution();
   };
-  
+
   function loadAirPollution() {
-  $scope.loading = true;
-  AirPolutionService.getAirPolutionByCity($scope.selectedCity)
-  .then(function (response) {
-  $scope.airPollution = response.data; // Use airPollution to store AQI and level
-  $scope.loading = false;
-  })
-  .catch(function (error) {
-  console.error("Error fetching air pollution data:", error);
-  $scope.loading = false;
-  });
+    $scope.loading = true;
+    AirPolutionService.getAirPolutionByCity($scope.selectedCity)
+    .then(function (response) {
+      $scope.airPollution = response.data; // Use airPollution to store AQI and level
+      $scope.loading = false;
+    })
+    .catch(function (error) {
+      console.error("Error fetching air pollution data:", error);
+      $scope.loading = false;
+    });
   }
-  
+
   $scope.$watch("selectedCity", function () {
-  loadAirPollution();
+    loadAirPollution();
   });
-  });
+
+});
